c548691ad02f1711184c8b8a6e7953d47bd6b8e3
devking0102 <devking0102@outlook.com>
2024-04-08 00:45:04 -0700

    modify answerPreview and questionPreview

--- frontend/src/components/Answer/AnswerPreview.tsx
+++ frontend/src/components/Answer/AnswerPreview.tsx
@@ -2,22 +2,41 @@ import React from 'react';
 import { Observer } from 'mobx-react-lite';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import AnswerActions from './AnswerActions';
+import { useState } from 'react';
 
 const AnswerPreview: React.FC<any> = props => {
+  const [showMore, setShowMore] = useState(false)
   return <Observer>{() => {
     const { answer } = props;
-
+    const toggleShowMore = (e: any) => {
+      e.preventDefault()
+      setShowMore(!showMore)
+    }
     return (
       <div className="answer-preview bg-info bg-opacity-25 mb-3 p-3 rounded-2">
         <div className="answer-meta">
-          <h3 className='text-dark blockquote'>Title: {answer.title}</h3>
+          <h3 className='text-dark blockquote text-break'>Title: {answer.title}</h3>
 
           <div className="info">
             <h5 className="author text-dark-emphasis">
               Author: {answer.user.firstName} {answer.user.lastName}
             </h5>
-            <h6 className='text-secondary'>content: {answer.content}</h6>
-            <p className='text-success'>Question: {answer.question.title}</p>
+            {answer.content.length > 200 ? 
+              <div>
+                <h6 className='text-secondary text-break'>content: {showMore ? answer.content : (answer.content.substr(0, 200) + '...')}</h6>
+                <div className='d-flex justify-content-end'>
+                  <li className='list-unstyled' onClick={toggleShowMore}>
+                    {
+                      showMore ? <a href="#">Show less</a> : <a href="#">Show more</a>
+                    }
+                  </li>
+                </div>
+              </div> : 
+              <div>
+                <h6 className='text-secondary text-break'>content: {answer.content}</h6>
+              </div>
+            }
+            <p className='text-success text-break'>Question: {answer.question.title}</p>
             <div className='d-flex justify-content-between'>
               <AnswerActions onDelete={props.onDelete} answer={answer}/>
               <span className="date text-info-emphasis pull-right">
--- frontend/src/components/Question/QuestionPreview.tsx
+++ frontend/src/components/Question/QuestionPreview.tsx
@@ -2,18 +2,37 @@ import React from 'react';
 import { Observer } from 'mobx-react-lite';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import QuestionActions from './QuestionActions';
+import { useState } from 'react';
 
 const QuestionPreview: React.FC<any> = props => {
+  const [showMore, setShowMore] = useState(false)
   return <Observer>{() => {
     const { question } = props;
-
+    const toggleShowMore = (e: any) => {
+      e.preventDefault()
+      setShowMore(!showMore)
+    }
     return (
       <div className="question-preview bg-primary-subtle bg-opacity-25 mb-3 p-3 rounded-2">
         <div className="question-meta">
-          <h3 className='text-dark blockquote'>Title: {question.title}</h3>
+          <h3 className='text-dark blockquote text-break'>Title: {question.title}</h3>
 
           <div className="info">
-            <p className='text-secondary fs-20'>content: {question.content}</p>
+          {question.content.length > 200 ? 
+              <div>
+                <h6 className='text-secondary text-break'>content: {showMore ? question.content : (question.content.substr(0, 200) + '...')}</h6>
+                <div className='d-flex justify-content-end'>
+                  <li className='list-unstyled' onClick={toggleShowMore}>
+                    {
+                      showMore ? <a href="#">Show less</a> : <a href="#">Show more</a>
+                    }
+                  </li>
+                </div>
+              </div> : 
+              <div>
+                <h6 className='text-secondary text-break'>content: {question.content}</h6>
+              </div>
+            }
             <div className='d-flex justify-content-between'>
               <QuestionActions onDelete={props.onDelete} question={question}/>
               <span className="date text-info-emphasis pull-right">
