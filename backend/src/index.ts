08a201b812c5a95061b3ba838d847c93f4c61749
devking0102 <devking0102@outlook.com>
2024-04-08 04:51:44 -0700

    add chalk

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,12 +2,13 @@ import app from "../app"
 import dotenv from "dotenv"
 dotenv.config()
 import mongoose from "mongoose"
+import chalk from "chalk"
 
 mongoose.connect('mongodb://127.0.0.1:27017/studentQA').then(() => {
-    console.log('MongoDB Connected!')
+    console.log(`${chalk.green('✓')} ${chalk.blue('MongoDB Connected!')}`)
 })
 const port = process.env.PORT || 5000
 
 app.listen(port, ():void => {
-    console.log(`Server is running at http://localhost:${port}`);
+    console.log(`${chalk.green('✓')} ${chalk.blue('Server is running at http://localhost:')}${port}`);
 })
